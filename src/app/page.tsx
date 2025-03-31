991ea8c12a8dd93ea42c0841d097284b2ff34990
Editor <harishij2063@gmail.com>
2025-04-01 00:04:04 +0530

    feat: T3 Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-center bg-gray-950">
+      <p className="text-2xl font-medium text-white">GDrive 101</p>
+    </main>
+  );
+}
